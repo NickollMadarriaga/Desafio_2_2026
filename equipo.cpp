cda62dc441d66727616e2e35a98a5c7f09574289
NickollMadarriaga <nickoll.madarriaga@udea.edu.co>
2026-04-23 18:48:56 -0500

    . cpp de la clase equipo
    Se completó la clase equipo

--- equipo.cpp
+++ equipo.cpp
@@ -1,3 +1,257 @@
 #include "equipo.h"
+#include <iostream>
 
-Equipo::Equipo() {}
+using namespace std;
+
+Equipo::Equipo() : ranking(0), jugadores(nullptr), estadisticas(nullptr) {
+    nombre[0] = '\0';
+    confederacion[0] = '\0';
+    jugadores   = new Jugador[MAX_JUGADORES];
+    estadisticas = new EstadisticasEquipo();
+}
+
+Equipo::Equipo(const char* nom, const char* conf, int rank, const EstadisticasEquipo& stats)
+    : ranking(rank) {
+
+    int i = 0;
+    while (i < 99 && nom[i] != '\0') {
+        nombre[i] = nom[i];
+        i++;
+    }
+    nombre[i] = '\0';
+
+    i = 0;
+    while (i < 49 && conf[i] != '\0') {
+        confederacion[i] = conf[i];
+        i++;
+    }
+    confederacion[i] = '\0';
+
+    jugadores = new Jugador[MAX_JUGADORES];
+    estadisticas = new EstadisticasEquipo(stats);
+}
+
+
+Equipo::Equipo(const Equipo& otro) : ranking(otro.ranking) {
+
+    int i = 0;
+    while (i < 99 && otro.nombre[i] != '\0') {
+        nombre[i] = otro.nombre[i];
+        i++;
+    }
+    nombre[i] = '\0';
+
+    i = 0;
+    while (i < 49 && otro.confederacion[i] != '\0') {
+        confederacion[i] = otro.confederacion[i];
+        i++;
+    }
+    confederacion[i] = '\0';
+
+    jugadores = new Jugador[MAX_JUGADORES];
+    for (int j = 0; j < MAX_JUGADORES; j++) {
+        jugadores[j] = otro.jugadores[j];
+    }
+
+    estadisticas = new EstadisticasEquipo(*otro.estadisticas);
+}
+
+Equipo::~Equipo() {
+    delete[] jugadores;
+    delete estadisticas;
+    jugadores   = nullptr;
+    estadisticas = nullptr;
+}
+
+const char* Equipo::getNombre() const { return nombre; }
+const char* Equipo::getConfederacion() const { return confederacion; }
+int Equipo::getRanking() const { return ranking; }
+Jugador* Equipo::getJugadores() const { return jugadores; }
+EstadisticasEquipo* Equipo::getEstadisticas() const { return estadisticas; }
+
+Jugador* Equipo::getJugador(int indice) const {
+    if (indice < 0 || indice >= MAX_JUGADORES) return nullptr;
+    return &jugadores[indice];
+}
+
+void Equipo::setNombre(const char* n) {
+
+    int i = 0;
+
+    while (i < 99 && n[i] != '\0') {
+        nombre[i] = n[i];
+        i++;
+    }
+
+    nombre[i] = '\0';
+}
+
+void Equipo::setConfederacion(const char* c) {
+
+    int i = 0;
+
+    while (i < 49 && c[i] != '\0') {
+        confederacion[i] = c[i];
+        i++;
+    }
+
+    confederacion[i] = '\0';
+}
+
+void Equipo::setRanking(int r) { ranking = r; }
+
+void Equipo::inicializarJugadores() {
+    int golesTotal  = estadisticas->getGolesFavor();
+    int golesPorJug = golesTotal / MAX_JUGADORES;
+    int resto       = golesTotal % MAX_JUGADORES;
+
+    char nomBuf[50], apeBuf[50];
+
+    for (int i = 0; i < MAX_JUGADORES; i++) {
+        int camisa = i + 1;
+        int k = 0;
+
+        // Copiar nombre
+        const char baseNom[] = "nombre";
+        int j = 0;
+        while (baseNom[j] != '\0') {
+            nomBuf[k++] = baseNom[j++];
+        }
+
+        // Convertir número a texto
+        int temp = camisa;
+        char num[10];
+        int len = 0;
+
+        if (temp == 0) {
+            num[len++] = '0';
+        } else {
+            while (temp > 0) {
+                num[len++] = (temp % 10) + '0';
+                temp /= 10;
+            }
+        }
+
+        for (int m = len - 1; m >= 0; m--) {
+            nomBuf[k++] = num[m];
+        }
+
+        nomBuf[k] = '\0';
+
+        k = 0;
+
+        const char baseApe[] = "apellido";
+        j = 0;
+        while (baseApe[j] != '\0') {
+            apeBuf[k++] = baseApe[j++];
+        }
+
+        temp = camisa;
+        len = 0;
+
+        if (temp == 0) {
+            num[len++] = '0';
+        } else {
+            while (temp > 0) {
+                num[len++] = (temp % 10) + '0';
+                temp /= 10;
+            }
+        }
+
+        for (int m = len - 1; m >= 0; m--) {
+            apeBuf[k++] = num[m];
+        }
+
+        apeBuf[k] = '\0';
+
+        int golesJug = golesPorJug + (i < resto ? 1 : 0);
+        EstadisticasJugador stats(0, golesJug, 0, 0, 0, 0, 0);
+        jugadores[i] = Jugador(nomBuf, apeBuf, camisa, stats);
+    }
+}
+
+int* Equipo::seleccionarOnce() const {
+    int* indices = new int[MAX_JUGADORES];
+    for (int i = 0; i < MAX_JUGADORES; i++) indices[i] = i;
+
+    for (int i = MAX_JUGADORES - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int tmp = indices[i]; indices[i] = indices[j]; indices[j] = tmp;
+    }
+
+    int* once = new int[11];
+    for (int i = 0; i < 11; i++) once[i] = indices[i];
+    delete[] indices;
+    return once;
+}
+
+EstadisticasEquipo* Equipo::getEstadisticasEquipo() const { return estadisticas; }
+
+void Equipo::actualizarEstadisticas(int gf, int gc, int amarillas, int rojas, int faltas,
+                                    bool gano, bool empato) {
+    estadisticas->setGolesFavor(estadisticas->getGolesFavor() + gf);
+    estadisticas->setGolesContra(estadisticas->getGolesContra() + gc);
+    estadisticas->setAmarillas(estadisticas->getAmarillas() + amarillas);
+    estadisticas->setRojas(estadisticas->getRojas() + rojas);
+    estadisticas->setFaltas(estadisticas->getFaltas() + faltas);
+    if (gano) estadisticas->setGanados(estadisticas->getGanados() + 1);
+    else if (empato) estadisticas->setEmpatados(estadisticas->getEmpatados() + 1);
+    else estadisticas->setPerdidos(estadisticas->getPerdidos() + 1);
+}
+
+bool Equipo::operator<(const Equipo& otro) const {
+    return ranking < otro.ranking;
+}
+
+bool Equipo::operator==(const Equipo& otro) const {
+    int i = 0;
+    while (nombre[i] != '\0' && otro.nombre[i] != '\0') {
+        if (nombre[i] != otro.nombre[i]) {
+            return false;
+        }
+        i++;
+    }
+
+    // Verificar que ambos hayan terminado al mismo tiempo
+    return nombre[i] == '\0' && otro.nombre[i] == '\0';
+}
+
+Equipo& Equipo::operator=(const Equipo& otro) {
+    if (this != &otro) {
+
+        int i = 0;
+        while (i < 99 && otro.nombre[i] != '\0') {
+            nombre[i] = otro.nombre[i];
+            i++;
+        }
+        nombre[i] = '\0';
+
+        i = 0;
+        while (i < 49 && otro.confederacion[i] != '\0') {
+            confederacion[i] = otro.confederacion[i];
+            i++;
+        }
+        confederacion[i] = '\0';
+
+        ranking = otro.ranking;
+
+        for (int j = 0; j < MAX_JUGADORES; j++) {
+            jugadores[j] = otro.jugadores[j];
+        }
+
+        delete estadisticas;
+        estadisticas = new EstadisticasEquipo(*otro.estadisticas);
+    }
+    return *this;
+}
+
+
+void Equipo::mostrar() const {
+    cout << nombre << " (" << confederacion << ") Ranking:" << ranking << endl;
+    estadisticas->mostrar();
+}
+
+void Equipo::mostrarConJugadores() const {
+    mostrar();
+    for (int i = 0; i < MAX_JUGADORES; i++) jugadores[i].mostrar();
+}
